3b870cfe5f5ade4079527b1dfd18f28dce1a8729
wei guo <wei.guo@aptiv.com>
2025-11-12 17:07:23 -0500

    VMC buck worked low side disabled for debug

--- f280015x_rpr/launchxl_ex1_f2800157_demo.c
+++ f280015x_rpr/launchxl_ex1_f2800157_demo.c
@@ -71,10 +71,59 @@
 #include "launchxl_ex1_ti_ascii.h"
 #include "driverlib.h"
 #include "device.h"
-
+#include <bitfield_support/f280015x_pievect.h>
+//#include <bitfield_support/f280015x_device.h>     // f280015x Headerfile Include File
+#include <buck_hal.h>
+#include <buck.h>
 //
 // Defines
 //
+
+//
+//---  State Machine Related ---
+//
+uint16_t vTimer0[4];         // Virtual Timers based on CPU Timer 0 (A events)
+uint16_t vTimer1[4];         // Virtual Timers based on CPU Timer 1 (B events)
+uint16_t vTimer2[4];         // Virtual Timers based on CPU Timer 2 (C events)
+
+//
+// Variable declarations for state machine
+//
+void (*Alpha_State_Ptr)(void); // Base States pointer
+void (*A_Task_Ptr)(void);      // State pointer A branch
+void (*B_Task_Ptr)(void);      // State pointer B branch
+void (*C_Task_Ptr)(void);      // State pointer C branch
+
+//
+// State Machine function prototypes
+//------------------------------------
+// Alpha states
+//
+void A0(void);  //state A0
+void B0(void);  //state B0
+void C0(void);  //state C0
+
+//
+// A branch states
+//
+void A1(void);  //state A1
+void A2(void);  //state A2
+void A3(void);  //state A3
+
+//
+// B branch states
+//
+void B1(void);  //state B1
+void B2(void);  //state B2
+void B3(void);  //state B3
+
+//
+// C branch states
+//
+void C1(void);  //state C1
+void C2(void);  //state C2
+void C3(void);  //state C3
+
 
 //
 // Select which SCI/UART pinout is desired -
@@ -338,6 +387,9 @@ void initADCSOCs(void)
     ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
 }
 
+
+
+uint16_t EN91=0;
 //
 // Main
 //
@@ -350,17 +402,86 @@ void main()
     //
     // Initialize device clock and peripherals
     //
-    Device_init();
+    BUCK_HAL_setupDevice();
+//    Device_init();
 
     //
     // Disable pin locks and enable internal pullups.
     //
     Device_initGPIO();
+    BUCK_HAL_setupSyncBuckPinsGpio();
+    BUCK_HAL_setupActiveLoadPinGPIO();
 
+
+    // Configure the run LED GPIO
+    //
+//    BUCK_HAL_setupRunLed();   CAN
+    BUCK_HAL_setup91Enable();
+
+    //
+    // Initialize global variables used in solution
+    //
+    BUCK_initUserVariables();
+    BUCK_initProgramVariables();
+
+
+    //
+    // Configure DCL and SFRA libraries
+    //
+    BUCK_setupDcl();
+    BUCK_setupSfra();
+    BUCK_setupSfraGui();
+    //
+    // Configure and enable system interrupt
+    //
+    PieVectTable.ADCA1_INT=0;
+    BUCK_HAL_setupInterrupt();
+
+
+
+    //
+    // Tasks State-machine initialization
+    //
+    Alpha_State_Ptr = &A0;
+    A_Task_Ptr = &A1;
+    B_Task_Ptr = &B1;
+    C_Task_Ptr = &C1;
+
+    //
+    // Stop ePWM clocks
+    //
+    BUCK_HAL_disableEpwmCounting();
+
+    //
+    // Set up ePWM for synchronous buck and active load control.
+    // Include comparator monitoring of the inductor current feedback
+    // signal (ILFB) for over-current trip protection.
+    //
+    BUCK_HAL_setupSyncBuckPwm();
+    BUCK_HAL_setupSyncBuckTripAction();
+    BUCK_HAL_setupActiveLoadPWM();
+    BUCK_setupHrpwmMepScaleFactor();
+
+
+    //
+    // Configure the embedded ADC to sample Vin, Vout, ILFB, and ILFB_AVG
+    //
+    BUCK_HAL_setupAdc();
+    BUCK_HAL_setupAdcTrigger();
+    //
+    // Start ePWM clocks
+    //
+    BUCK_HAL_enableEpwmCounting();
+
+    //
+    // Switch actuation pins over to ePWM function
+    //
+    BUCK_HAL_setupSyncBuckPinsEpwm();
+    BUCK_HAL_setupActiveLoadPinEPWM();
     //
     // For this example, only init the pins for the SCI-A port.
     //
-    EALLOW;
+//    EALLOW;
 #if !SCIPinout
     //
     // GPIO28 is the SCI Rx pin.
@@ -403,13 +524,13 @@ void main()
     //
     // Initialize PIE and clear PIE registers. Disables CPU interrupts.
     //
-    Interrupt_initModule();
+//    Interrupt_initModule();
 
     //
     // Initialize the PIE vector table with pointers to the shell Interrupt
     // Service Routines (ISR).
     //
-    Interrupt_initVectorTable();
+//    Interrupt_initVectorTable();
 
     //
     // Initialize SCIA
@@ -449,48 +570,48 @@ void main()
     //
     // Set up ADCs, initializing the SOCs to be triggered by software
     //
-    initADCs();
-    initADCSOCs();
+//    initADCs();
+//    initADCSOCs();
 
-    //
-    // Redirect STDOUT to SCI
-    //
-    status = add_device("scia", _SSA, SCI_open, SCI_close, SCI_read, SCI_write,
-                        SCI_lseek, SCI_unlink, SCI_rename);
-    fid = fopen("scia","w");
-    freopen("scia:", "w", stdout);
-    setvbuf(stdout, NULL, _IONBF, 0);
+//    //
+//    // Redirect STDOUT to SCI
+//    //
+//    status = add_device("scia", _SSA, SCI_open, SCI_close, SCI_read, SCI_write,
+//                        SCI_lseek, SCI_unlink, SCI_rename);
+//    fid = fopen("scia","w");
+//    freopen("scia:", "w", stdout);
+//    setvbuf(stdout, NULL, _IONBF, 0);
 
-    //
-    // Print a TI Logo to STDOUT
-    //
-    drawTILogo();
-
-    //
-    // Twiddle LEDs
-    //
-    GPIO_writePin(DEVICE_GPIO_PIN_LED1, 0);
-    GPIO_writePin(DEVICE_GPIO_PIN_LED2, 1);
-
-    for(i = 0; i < 50; i++)
-    {
-        GPIO_togglePin(DEVICE_GPIO_PIN_LED1);
-        GPIO_togglePin(DEVICE_GPIO_PIN_LED2);
-        DEVICE_DELAY_US(50000);
-    }
-
-    //
-    // LEDs off
-    //
-    GPIO_writePin(DEVICE_GPIO_PIN_LED1, 1);
-    GPIO_writePin(DEVICE_GPIO_PIN_LED2, 1);
-
-    //
-    // Clear out one of the text boxes so we can write more info to it
-    //
-    clearTextBox();
-
-    currentSample = sampleADC();
+//    //
+//    // Print a TI Logo to STDOUT
+//    //
+//    drawTILogo();
+//
+//    //
+//    // Twiddle LEDs
+//    //
+//    GPIO_writePin(DEVICE_GPIO_PIN_LED1, 0);
+//    GPIO_writePin(DEVICE_GPIO_PIN_LED2, 1);
+//
+//    for(i = 0; i < 50; i++)
+//    {
+//        GPIO_togglePin(DEVICE_GPIO_PIN_LED1);
+//        GPIO_togglePin(DEVICE_GPIO_PIN_LED2);
+//        DEVICE_DELAY_US(50000);
+//    }
+//
+//    //
+//    // LEDs off
+//    //
+//    GPIO_writePin(DEVICE_GPIO_PIN_LED1, 1);
+//    GPIO_writePin(DEVICE_GPIO_PIN_LED2, 1);
+//
+//    //
+//    // Clear out one of the text boxes so we can write more info to it
+//    //
+//    clearTextBox();
+//
+//    currentSample = sampleADC();
 
     //
     // Main program loop - continually sample temperature
@@ -498,35 +619,293 @@ void main()
     for(;;)
     {
         //
-        // Sample ADCINA6
+        // Background state machine entry & exit point
         //
-        currentSample = sampleADC();
-
-        //
-        // Update the serial terminal output
-        //
-        updateDisplay();
-
-        //
-        // If the sample is above midscale light one LED
-        //
-        if(currentSample > 2048)
-        {
-            GPIO_writePin(DEVICE_GPIO_PIN_LED1, 0);
-            GPIO_writePin(DEVICE_GPIO_PIN_LED2, 1);
-        }
-        else
-        {
-            //
-            // Otherwise light the other
-            //
-            GPIO_writePin(DEVICE_GPIO_PIN_LED1, 1);
-            GPIO_writePin(DEVICE_GPIO_PIN_LED2, 0);
-        }
-
-        DEVICE_DELAY_US(1000000);
+        (*Alpha_State_Ptr)();   // jump to an Alpha state (A0,B0,...)
+        GPIO_writePin(EN91_GPIO, EN91);
+//        //
+//        // Sample ADCINA6
+//        //
+//        currentSample = sampleADC();
+//
+//        //
+//        // Update the serial terminal output
+//        //
+//        updateDisplay();
+//
+//        //
+//        // If the sample is above midscale light one LED
+//        //
+//        if(currentSample > 2048)
+//        {
+//            GPIO_writePin(DEVICE_GPIO_PIN_LED1, 0);
+//            GPIO_writePin(DEVICE_GPIO_PIN_LED2, 1);
+//        }
+//        else
+//        {
+//            //
+//            // Otherwise light the other
+//            //
+//            GPIO_writePin(DEVICE_GPIO_PIN_LED1, 1);
+//            GPIO_writePin(DEVICE_GPIO_PIN_LED2, 0);
+//        }
+//
+//        DEVICE_DELAY_US(1000000);
     }
+} //END MAIN CODE
+
+//
+// ISR1() interrupt function
+//
+uint16_t CntIsr1=0;
+#ifdef BUCK_CONTROL_RUNNING_ON_CPU
+#pragma CODE_SECTION(ISR1,"isrcodefuncs");
+#pragma INTERRUPT(ISR1, HPI)
+interrupt void ISR1(void)
+{
+    //
+    // ISR is triggered by the ADC every EPWM cycle after Vout is sampled.
+    // ADC should sample early enough in the cycle for the ISR to write
+    // the updated actuation values before the shadow loads take place.
+    //
+    CntIsr1++;
+    BUCK_runIsr();
 }
+
+#endif
+//
+//=============================================================================
+//  STATE-MACHINE SEQUENCING AND SYNCRONIZATION FOR SLOW BACKGROUND TASKS
+//=============================================================================
+//
+//
+//--------------------------------- FRAME WORK --------------------------------
+//
+void A0(void)
+{
+    //
+    // loop rate synchronizer for A-tasks
+    //
+    if(GET_TASKA_TIMER_OVERFLOW_STATUS == 1)
+    {
+        CLEAR_TASKA_TIMER_OVERFLOW_FLAG;    // clear flag
+
+        //
+        // jump to an A Task (A1,A2,A3,...)
+        //
+        (*A_Task_Ptr)();
+
+        vTimer0[0]++;           // virtual timer 0, instance 0 (spare)
+    }
+    Alpha_State_Ptr = &B0;      // Comment out to allow only A tasks
+}
+
+void B0(void)
+{
+    //
+    // loop rate synchronizer for B-tasks
+    //
+    if(GET_TASKB_TIMER_OVERFLOW_STATUS  == 1)
+    {
+        CLEAR_TASKB_TIMER_OVERFLOW_FLAG;                // clear flag
+
+        //
+        // jump to an B Task (B1,B2,B3,...)
+        //
+        (*B_Task_Ptr)();
+
+        vTimer1[0]++;           // virtual timer 1, instance 0 (spare)
+    }
+
+    Alpha_State_Ptr = &C0;      // Allow C state tasks
+}
+
+void C0(void)
+{
+    //
+    // loop rate synchronizer for C-tasks
+    //
+    if(GET_TASKC_TIMER_OVERFLOW_STATUS  == 1)
+    {
+        CLEAR_TASKC_TIMER_OVERFLOW_FLAG;                // clear flag
+
+        //
+        // jump to an C Task (C1,C2,C3,...)
+        //
+        (*C_Task_Ptr)();
+
+        vTimer2[0]++;           // virtual timer 2, instance 0 (spare)
+    }
+
+    Alpha_State_Ptr = &A0;      // Return to A state tasks
+}
+
+//
+//=============================================================================
+//  A - TASKS (executed at 1kHz)
+//=============================================================================
+//
+void A1(void)
+{
+    //
+    // Calculate the effective ePWM duty or Vout setting based on a
+    // combination of user input and maximum slew rate allowed
+    //
+    #if(BUCK_LAB_NUMBER == BUCK_LAB_OPEN_LOOP_VMC)
+        BUCK_updateDutyTarget();
+        BUCK_updateDutySlewed();
+    #elif(BUCK_LAB_NUMBER == BUCK_LAB_OPEN_LOOP_PCMC)
+        BUCK_updateDacTarget();
+        BUCK_updateDacSlewed();
+    #else
+        BUCK_updateVoutTarget();
+        BUCK_updateVoutSlewed();
+    #endif
+
+    //
+    // Execute task A2 the next time CpuTimer0 decrements to 0
+    //
+    A_Task_Ptr = &A2;
+}
+
+void A2(void)
+{
+    //
+    // Service SCI link for SFRA GUI
+    //
+    BUCK_SFRA_GUI_RUN_COMMS(&BUCK_sfra);
+
+    //
+    // Execute task A3 the next time CpuTimer0 decrements to 0
+    //
+    A_Task_Ptr = &A3;
+}
+
+void A3(void)
+{
+    //
+    // Calibrate HRPWM MEP Scale Factor
+    //
+    BUCK_updateHrpwmMepScaleFactor();
+
+    //
+    // Execute task A1 the next time CpuTimer0 decrements to 0
+    //
+    A_Task_Ptr = &A1;
+}
+
+
+
+static void blueLedToggle(void)
+{
+    static uint32_t counter = 0;
+
+    counter++;
+    GPIO_writePin(DEVICE_GPIO_PIN_LED1, counter & 1);
+}
+//
+//=============================================================================
+//  B - TASKS (executed at 100Hz)
+//=============================================================================
+//
+void B1(void)
+{
+    //
+    // Toggle on-board LED to indicate program execution
+    //
+    blueLedToggle();
+//    BUCK_HAL_toggleRunLed();
+
+    //
+    // Execute task B2 the next time CpuTimer1 decrements to 0
+    //
+    B_Task_Ptr = &B2;
+}
+
+void B2(void)
+{
+    //
+    // Manage SFRA sweep
+    //
+    BUCK_SFRA_RUN_BACKGROUND(&BUCK_sfra);
+
+    //
+    // Execute task B3 the next time CpuTimer1 decrements to 0
+    //
+    B_Task_Ptr = &B3;
+}
+
+void B3(void)
+{
+    //
+    // SPARE
+    //
+
+    //
+    // Execute task B1 the next time CpuTimer1 decrements to 0
+    //
+    B_Task_Ptr = &B1;
+}
+
+//
+//=============================================================================
+//  C - TASKS (executed at 10Hz)
+//=============================================================================
+//
+void C1(void)
+{
+    //
+    // Update user variables to show the state of over-current trip status
+    // and most recent ADC readings of Vin, Vout, ILFB, and ILFB_AVG
+    //
+    BUCK_updateOverCurrentStatus();
+    BUCK_updateSensedValues();
+
+    //
+    // Execute task C2 the next time CpuTimer2 decrements to 0
+    //
+    C_Task_Ptr = &C2;
+}
+
+void C2(void)
+{
+    //
+    // Update the over-current trip limit and PID coefficients based on
+    // user input variables
+    //
+    BUCK_updateOverCurrentLimit();
+    BUCK_updateControllerCoefficients();
+
+    //
+    // Execute task C3 the next time CpuTimer2 decrements to 0
+    //
+    C_Task_Ptr = &C3;
+}
+
+void C3(void)
+{
+    //
+    // Update the operation of the active load resistor based on
+    // user input variables
+    //
+    BUCK_updateActiveLoad();
+    BUCK_processIsrDutyUpdateMarginStats();
+    BUCK_updateVoutLog();
+
+    //
+    // Execute task C1 the next time CpuTimer2 decrements to 0
+    //
+    C_Task_Ptr = &C1;
+}
+
+
+
+
+
+//
+// End of buck_main.c
+//
+
 
 //
 // End of File
